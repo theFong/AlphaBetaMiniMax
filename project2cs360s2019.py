9a0e6321e7abf51fc9b741e80f8befa0535508dc
theFong <alecsanf@usc.edu>
2019-03-22 16:01:25 -0700

    working but long

--- project2cs360s2019.py
+++ project2cs360s2019.py
@@ -31,62 +31,80 @@ def main():
 
     write_output(next_hero, "output.txt")
 
-def apply_minimax(heroes):
-    return minimax(heroes, True)[1]
+def apply_minimax(heroes_df):
+    available = set(get_available_players(heroes_df).index.tolist())
+    max_heroes = set(get_selected_players(heroes_df, 1).index.tolist())
+    min_heroes = set(get_selected_players(heroes_df, 2).index.tolist())
 
-def minimax(heroes, isMaxPlayer):
-    if get_num_selected_players(heroes) == 10: # if at leaf node #TODO possible bugs exist here
-        return calc_advantage(heroes), 0
+    return minimax(heroes_df[["power", "mastery_i", "mastery_j"]], max_heroes, min_heroes, available, True)[1]
 
-    available_players = get_available_players(heroes)
+def minimax(heroes, max_heroes, min_heroes, available_players, isMaxPlayer):
+    if get_num_selected_players(max_heroes, min_heroes) == 10:
+        return calc_advantage(heroes, max_heroes, min_heroes), 0
+
     if isMaxPlayer:
         max_value = float("-inf")
         max_hero = 0
-        for i, _ in available_players.iterrows():
-            nf = heroes.copy()
-            nf.at[i, "team_id"] = 1
-            val, _ = minimax(nf, not isMaxPlayer)
+
+        for i in available_players:
+
+            available_players.remove(i)
+            max_heroes.add(i)
+
+            val, _ = minimax(heroes, max_heroes, min_heroes, available_players, not isMaxPlayer)
             # max
             if val > max_value:
                 max_hero = i
                 max_value = val
+            elif val == max_value and i < max_hero:
+                max_hero = i
+
+            available_players.add(i)
+            max_heroes.remove(i)
+
         return max_value, max_hero
     else:
         min_value = float("inf")
         min_hero = 0
-        for i, _ in available_players.iterrows():
-            nf = heroes.copy()
-            nf.at[i, "team_id"] = 2
-            val, _ = minimax(nf, not isMaxPlayer)
+
+        for i in available_players:
+
+            available_players.remove(i)
+            min_heroes.add(i)
+
+            val, _ = minimax(heroes, max_heroes, min_heroes, available_players, not isMaxPlayer)
             # min
             if val < min_value:
                 min_value = val
                 min_hero = i
+            elif val == min_value and i < min_hero:
+                min_hero = i
+
+            available_players.add(i)
+            min_heroes.remove(i)
+
         return min_value, min_hero
 
-def get_selected_players(heroes_df, isMaxPlayer):
-    player_id = 1 if isMaxPlayer else 2
-    return heroes_df.loc[heroes_df["team_id"] == player_id]
+def get_selected_players(heroes_df, id):
+    return heroes_df.loc[heroes_df["team_id"] == id]
 
 # this may need to be optimized
-def get_num_selected_players(heroes_df):
-    num = len(heroes_df.loc[heroes_df["team_id"] == 1]) + len(heroes_df.loc[heroes_df["team_id"] == 2])
-    if num == 8 or num == 9 or num == 10:
-        print(heroes_df)
+def get_num_selected_players(max_heroes, min_heroes):
+    num = len(max_heroes) + len(min_heroes)
+    # num = len(heroes_df.loc[heroes_df["team_id"] == 1]) + len(heroes_df.loc[heroes_df["team_id"] == 2])
     return num
 
 # this may need to be optimized
 def get_available_players(heroes_df):
     return heroes_df.loc[heroes_df["team_id"] == 0]
         
-def calc_advantage(heroes_df):
+def calc_advantage(heroes_df, max_heroes, min_heroes):
     # advatage = (synergy_bonus_max + sum(mastery_i_max * power_i_max) - (synergy_bonus_min + sum(mastery_i_min * power_i_min))
-    return calc_player_advantage(heroes_df, 1) - calc_player_advantage(heroes_df, 2) 
+    return calc_player_advantage(heroes_df, max_heroes, 1) - calc_player_advantage(heroes_df, min_heroes, 2) 
 
-def calc_player_advantage(heroes_df, player_id):
+def calc_player_advantage(heroes_df, heroes_selected, player_id):
     # (synergy_bonus + sum(mastery_i * power_i)
-    heroes_selected = heroes_df.loc[heroes_df["team_id"] == player_id]
-    synergy_bonus = 120 if calc_synergy(heroes_selected) else 0
+    synergy_bonus = calc_synergy(heroes_selected)
 
     mastery_column = ""
     if player_id == 1:
@@ -96,19 +114,20 @@ def calc_player_advantage(heroes_df, player_id):
     else:
         raise(Exception("Invalid player id when calculating advantage"))
 
-    mastery = heroes_selected[[mastery_column]]
-    power = heroes_selected[["power"]]
+    selected = heroes_df.loc[ heroes_selected ]
+    mastery = selected[[mastery_column]]
+    power = selected[["power"]]
 
     return synergy_bonus + mastery.T.dot(power).iloc[0,0]
 
 def calc_synergy(heroes_selected):
     last_set = set()
-    for i in heroes_selected.index:
+    for i in heroes_selected:
         last = str(i)[-1]
         if last in last_set:
-            return False
+            return 0
         last_set.add(last)
-    return True
+    return 120
 
 def apply_alpha_beta_pruning(heroes):
     return 0
